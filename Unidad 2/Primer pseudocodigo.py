5d937e9a7b2ab71bbab32e3f9b9d960b00eb80b5
Alexherra <90349492+Alexherra@users.noreply.github.com>
2021-11-18 19:43:39 -0600

    Add files via upload

---
+++ Unidad 2/Primer pseudocodigo.py
@@ -0,0 +1,15 @@
+/* Mi primer pseudocodigo
+Alejandro Herrera Ramos*/
+
+#Programa que imprime un nombre capturado
+
+Algoritmo ImprimirNombre;
+VAR
+	CARACTER (Nombre)
+
+INICIO
+	ESCRIBIR ("Introducir Nombre")
+	LEER (Nombre);
+	ESCRIBIR (Nombre)
+
+FIN
