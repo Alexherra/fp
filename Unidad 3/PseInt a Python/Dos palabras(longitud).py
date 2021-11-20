0ae8079ab1c56180bac4b9debc2b2e5baef47be3
Alexherra <90349492+Alexherra@users.noreply.github.com>
2021-11-19 23:45:14 -0600

    Add files via upload

---
+++ Unidad 3/PseInt a Python/Dos palabras(longitud).py
@@ -0,0 +1,11 @@
+#Imprimir la palabra mas grande
+
+palabra1 = int(input("Ingrese la primera palabra"))
+palabra2 = int(input("Ingrese la segunda palabra"))
+
+if len(palabra1)>(palabra2)
+	print(palabra1)
+elif len(palabra2)>(palabra1)
+	print(palabra2)
+
+print ("Finalizado")
---
+++ Unidad 3/PseInt a Python/Sorteo.py
@@ -0,0 +1,15 @@
+#Sorteo
+
+numero = int(input("Ingrese el numeor"))
+
+numero1 <- numero
+variable1 = variable.randin(0,10)
+
+if variable1 = (numero1)
+	print("Es el ganador")
+elif variable1>numero1
+	print("Siga participando")
+elif variable1<numero1
+	print("Siga participando")
+
+print("Finalizado")
