03a41d870d296a50d6f4cafdd04ffd757e9bca80
Alexherra <90349492+Alexherra@users.noreply.github.com>
2021-12-01 11:35:55 -0600

    Add files via upload

---
+++ Unidad 4/Trabajo en clase 3 (Inversion).py
@@ -0,0 +1,13 @@
+#Programa para poder determinar la cantidad de ganacia de una inversion anual
+
+print ("Buen dia por favor ingrese la cantidad que desea invertir")
+dinero=int(print("Ingrese aqui la cantidad"))
+print ("Ahora ingrese el interes anual con la cual va a invetir de forma aunal")
+interes=int(print("Ingrese aui el valor del interes"))
+printv("Y para finalizar ingrese cuantos años va a realiza la inversion")
+años=int(print("Ingrese aqui los años"))
+
+inversion = años=(dinero+(interes/100))
+
+print("El valor final de su inverion serian $"inversion)
+print("La operacion a finalizado")
