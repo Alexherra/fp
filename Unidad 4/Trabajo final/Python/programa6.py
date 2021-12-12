e4025970570f26dd6cb51353aa7586abc9b21c22
Alexherra <90349492+Alexherra@users.noreply.github.com>
2021-12-11 19:27:53 -0600

    Add files via upload

---
+++ Unidad 4/Trabajo final/Python/programa6.py
@@ -0,0 +1,18 @@
+#Programa que mide el tiempo de llegasa a un destino segun los kilometros a recorrer
+
+print("")
+print("Programa que mide el tiempo de llegada")
+print("")
+
+print("Ingrese primero los kilometros a recorrer")
+kilo=float(input("Ingreselos aqui "))
+print("Ahora ingrese la velocidad en kilometros por hora")
+velo=float(input("Ingreselos aqui "))
+
+total= kilo*velo
+total2=total
+
+print("El tiempo estimado de llegada a su destino seria de ",total2,"hrs")
+print("")
+
+print("El Programa a finalizado")
